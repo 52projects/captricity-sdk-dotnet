d1c35ddc4e15df9cbe8d85988bebad733c924000
Chad Meyer <chadmeyer@me.com>
2014-10-10 00:58:06 -0500

    tweaks the box and field sets.

--- Captricity.API/Sets/BoxSet.cs
+++ Captricity.API/Sets/BoxSet.cs
@@ -7,16 +7,14 @@ using Restify;
 using Captricity.API.Model;
 using RestSharp;
 namespace Captricity.API.Sets {
-   public  class BoxSet : ApiSet<Box> {
-               private const string LIST_URL = "/v1/box";
+    public class BoxSet : ApiSet<Box> {
+        private const string LIST_URL = "/v1/box";
         private const string GET_URL = "/v1/box/{0}";
-        private const string SUBMIT_URL = "/v1/batch/{0}/submit";
-        private const string PRICE_URL = "/v1/batch/{0}/price";
-        private const string READINESS_URL = "/v1/batch/{0}/readiness";
 
         public BoxSet(IDictionary<string, string> headers, string baseURl) : base(headers, baseURl, ContentType.JSON) { }
 
         protected override string GetUrl { get { return GET_URL; } }
         protected override string ListUrl { get { return LIST_URL; } }
+        protected override string EditUrl { get { return GET_URL; } }
     }
 }
--- Captricity.API/Sets/FieldSet.cs
+++ Captricity.API/Sets/FieldSet.cs
@@ -13,14 +13,13 @@ namespace Captricity.API.Sets {
         private const string CHILD_LIST_URL = "/v1/sheet/{0}/field";
         private const string CHILD_GET_URL = "/v1/sheet/{0}/field/{1}";
         private const string CREATE_URL = "/v1/sheet/{0}/field/";
-        private const string UPDATE_URL = "/v1/field/{0}";
 
         public FieldSet(IDictionary<string, string> headers, string baseURl) : base(headers, baseURl, ContentType.JSON) { }
 
         protected override string GetUrl { get { return GET_URL; } }
         protected override string GetChildListUrl { get { return CHILD_LIST_URL; } }
         protected override string GetChildUrl { get { return CHILD_GET_URL; } }
-        protected override string EditUrl { get { return UPDATE_URL; } }
+        protected override string EditUrl { get { return GET_URL; } }
 
         public Field Create(int sheetID, Field entity) {
             entity.SheetID = sheetID;
